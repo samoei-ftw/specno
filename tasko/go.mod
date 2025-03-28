7de95ea145c72bcd665bb0e08d0b290c364e141e
Samoei <samoeiayuka@gmail.com>
2025-03-28 19:06:17 +0200

    feat(): Enrich jwt helper to create and validate tokens

--- tasko/go.mod
+++ tasko/go.mod
@@ -5,10 +5,14 @@ go 1.22.0
 toolchain go1.24.1
 
 require (
+	github.com/golang-jwt/jwt/v4 v4.5.2
+	github.com/lib/pq v1.10.9
+)
+
+require (
 	github.com/golang-migrate/migrate/v4 v4.18.2 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/lib/pq v1.10.9
 	go.uber.org/atomic v1.7.0 // indirect
 )
--- tasko/pkg/auth/jwt.go
+++ tasko/pkg/auth/jwt.go
@@ -6,14 +6,78 @@
 // This script is responsible for JWT generation
 package auth
 
-import "errors"
+import (
+	"errors"
+	"log"
+	"os"
+	"time"
 
-// TODO: Implement JWT generation and validation functions
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/joho/godotenv"
+)
 
-func GenerateToken() (string, error) {
-	return "", errors.New("not implemented")
+var jwtKey []byte
+
+func init() {
+	//load env
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Println("Warning")
+	}
+
+	// get jwt secret key
+	secret := os.Getenv("JWT_SECRET")
+	if secret == "" {
+		log.Fatal("JWT_SECRET not set in environment variables")
+	}
+	jwtKey = []byte(secret)
 }
 
-func ValidateToken(token string) (bool, error) {
-	return false, errors.New("not implemented")
+type Claims struct {
+	UserID int `json:"user_id"`
+	jwt.RegisteredClaims
+}
+
+// GenerateJWT generates a new JWT token
+func GenerateToken(userID int) (string, error) {
+	claims := Claims{
+		UserID: userID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtKey)
+}
+
+// ParseJWT parses the JWT token
+func ParseJWT(tokenStr string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenStr, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(*Claims)
+	if !ok || !token.Valid {
+		return nil, err
+	}
+	return claims, nil
+}
+
+// checks if the JWT token is valid
+func ValidateToken(tokenStr string) (bool, error) {
+	claims, err := ParseJWT(tokenStr)
+	if err != nil {
+		return false, err
+	}
+
+	// Check expiration
+	if claims.ExpiresAt.Time.Before(time.Now()) {
+		return false, errors.New("token has expired")
+	}
+
+	return true, nil
 }
