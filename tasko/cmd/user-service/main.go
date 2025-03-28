a1bb6c9cf18583259101a9c5f33bf75c4de13480
Samoei <samoeiayuka@gmail.com>
2025-03-28 19:26:05 +0200

    fix(): DSN assigning

--- tasko/cmd/user-service/main.go
+++ tasko/cmd/user-service/main.go
@@ -9,15 +9,36 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/samoei-ftw/tasko/config"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+var db *gorm.DB
+
 func main() {
-	fmt.Println("Running container on port 8080...")
-	// Load environment variables
+	err := godotenv.Load()
 	config.Load()
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	// Create the DSN (Data Source Name) string
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", dbHost, dbUser, dbPassword, dbName, dbPort)
+	// Connect to the database
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to the database:", err)
+	}
+	fmt.Println("Running container on port 8080...")
+
 	for {
 		time.Sleep(10 * time.Second) // keep container alive
 	}
--- tasko/internal/models/user.go
+++ tasko/internal/models/user.go
@@ -1,10 +1,22 @@
 package user
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
-	ID        int       `json:"id"`
+	ID        uint      `json:"id" gorm:"primaryKey"`
 	Email     string    `json:"email"`
 	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
+}
+
+func CreateUser(db *gorm.DB, user User) (uint, error) {
+	user.CreatedAt = time.Now()
+	if err := db.Create(&user).Error; err != nil {
+		return 0, err
+	}
+	return user.ID, nil
 }
