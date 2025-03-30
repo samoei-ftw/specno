ddf439f53ade0984d5a61b58b87cb8c435bfe42f
Samoei <samoeiayuka@gmail.com>
2025-03-30 14:09:10 +0200

    fix(): Env var loading fix

--- tasko/cmd/user-service/main.go
+++ tasko/cmd/user-service/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -13,7 +14,9 @@ import (
 
 func main() {
 	// Load environment variables
-	err := godotenv.Load()
+	err := godotenv.Load(".env")
+	fmt.Println("DB_USER:", os.Getenv("DB_USER"))
+	fmt.Println("DB_PASSWORD:", os.Getenv("DB_PASSWORD"))
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
