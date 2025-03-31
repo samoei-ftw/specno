b82e856ca0284c9f13090770d14df4add6adfea2
Samoei <samoeiayuka@gmail.com>
2025-03-31 23:10:26 +0200

    fix(): Script updates

--- backend/run.sh
+++ backend/run.sh
@@ -5,23 +5,36 @@
 #
 # This script sets up a docker container to run the backend project locally
 
-# Create a custom network if it doesn't exist
 docker network create specno-network || true
 
-# Start the PostgreSQL container (assuming it's already set up in run_db.sh)
-# You can either source the `run_db.sh` script here or just call it after ensuring it's been set up
+echo "Removing existing containers..."
+docker rm -f specno-user-service || true
+
+docker rm -f specno-postgres || true
+docker volume rm specno-postgres-data || true
+
+echo "Removing existing Docker image(s)..."
+docker rmi -f specno-user-service || true
+
+docker exec -it specno-db psql -U postgres -d postgres -c "
+DO \$\$ DECLARE
+    r RECORD;
+BEGIN
+    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP
+        EXECUTE 'TRUNCATE TABLE ' || quote_ident(r.tablename) || ' CASCADE';
+    END LOOP;
+END \$\$;"
+
 ./run_db.sh
 
-# Build the user service image
-echo "Building user service Docker image..."
+echo "Building Docker image..."
 docker build -t specno-user-service -f Dockerfile .
 
-# Run the user service container on the custom network
-echo "Starting user service container..."
+echo "Starting container..."
 docker run -d \
   -p 8080:8080 \
   --name specno-user-service \
   --network specno-network \
   specno-user-service
 
-echo "User service is running on port 8080."
+echo "Backend server is running on port 8080."
