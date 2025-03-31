052ffd3a189ad0ab8d8995f9d74c7cab82823528
Samoei <samoeiayuka@gmail.com>
2025-03-31 22:30:21 +0200

    feat(): Handle request

---
+++ frontend/src/api/auth.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.API_URL || "http://localhost:8080";
+
+export const registerUser = async (email: string, password: string) => {
+    const response = await axios.post(`${API_URL}/register`, { email, password });
+    return response.data;
+};
