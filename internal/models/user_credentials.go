6fa1f2ba022dad977db1fe811a34aa1057641069
Samoei <samoeiayuka@gmail.com>
2025-04-01 02:41:32 +0200

    chore(): Stabilise

--- internal/models/user_credentials.go
+++
@@ -1,8 +0,0 @@
-// UserCredentials is used for the login request body
-package models
-
-type UserCredentials struct {
-	ID       uint   `json:"id" gorm:"primaryKey"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
