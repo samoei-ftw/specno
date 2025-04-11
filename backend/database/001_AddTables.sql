fae78bcbd836e6f4d022940eac6aaa6e3d5acb2a
Samoei <samoeiayuka@gmail.com>
2025-04-11 15:27:00 +0200

    feat(): Add missing columns for Tasks entity

--- backend/database/001_AddTables.sql
+++ backend/database/001_AddTables.sql
@@ -22,9 +22,10 @@ CREATE TABLE IF NOT EXISTS projects (
 CREATE TABLE IF NOT EXISTS tasks (
     id SERIAL PRIMARY KEY,
     project_id INT NOT NULL,
+    user_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     description TEXT,
     due_date TIMESTAMP,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE
+    FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE,
 );
